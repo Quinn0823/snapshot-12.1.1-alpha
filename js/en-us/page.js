afd460b2d1b225c53b47568408047adcf74ff5c6
Quinn Qiu <quinnqiu0823@icloud.com>
2023-09-19 11:44:19 +0800

    Update page.js

--- js/en-us/page.js
+++ js/en-us/page.js
@@ -1,6 +1,6 @@
 var title = document.title;
 document.title = title + " | Quinn-Web";
-document.getElementById("header").innerHTML = "<div class='list'><ul><img src='images/logo.png'/><li><a href='snapshot-12.1.1-alpha/en-us/index.html'>Home</a></li><li><a href='snapshot-12.1.1-alpha/en-us/projects.html'>Projects</a></li><li><a href='snapshot-12.1.1-alpha/en-us/media.html'>Media</a><ul><li><a href='snapshot-12.1.1-alpha/en-us/media/music.html'>Music</a></li><li><a href='snapshot-12.1.1-alpha/en-us/media/photos.html'>Photos</a></li><li><a href='snapshot-12.1.1-alpha/en-us/media/videos.html'>Videos</a></li></ul></li><li class='about'><a href='snapshot-12.1.1-alpha/en-us/about.html'>About</a></li></ul></div><h1>" + title + "</h1><hr />";
+document.getElementById("header").innerHTML = "<div class='list'><ul><img src='images/logo.png'/><li><a href='index.html'>Home</a></li><li><a href='projects.html'>Projects</a></li><li><a href='media.html'>Media</a><ul><li><a href='media/music.html'>Music</a></li><li><a href='media/photos.html'>Photos</a></li><li><a href='media/videos.html'>Videos</a></li></ul></li><li class='about'><a href='about.html'>About</a></li></ul></div><h1>" + title + "</h1><hr />";
 document.getElementById("footer").innerHTML = "<div class='sel'><select id='lang' onchange='langchange(); language()'><option value='1' selected='selected'>United States - English</option><option value='2'>简体中文（中国大陆）</option></select></div><span style='text-align: center; display:block; '>📅 Last Update: July 23, 2022 | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " Quinn All Rights Reserved.</span>";
 
 var path = window.location.pathname;
@@ -8,8 +8,8 @@ path = path.substring(28);
 
 function langchange() {
     if(document.getElementById("lang").value=="1") {
-        window.location = "/snapshot-12.1.1-alpha/en-us" + path;
+        window.location = "en-us" + path;
     } else if(document.getElementById("lang").value == "2") {
-        window.location = "/snapshot-12.1.1-alpha/zh-cn" + path;
+        window.location = "zh-cn" + path;
     }
 }
